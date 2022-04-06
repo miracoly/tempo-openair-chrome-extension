f8c46da1d843879061a2f447716db0f331a93281
miracoly <68049792+miracoly@users.noreply.github.com>
2022-04-07 00:41:58 +0200

    add & save input for tempo issue key

--- src/background.ts
+++ src/background.ts
@@ -2,11 +2,15 @@ import { parseBoundaries, tabContains, TIMESHEET_TITLE, TIMESHEET_URL } from './
 import { Message, MessageType } from './messages';
 import { getDayReportsFromTempo } from './tempo/tempo';
 import { newDayRange } from './utils/utils';
+import { DayReport } from './tempo/types';
 import Tab = chrome.tabs.Tab;
 import MessageSender = chrome.runtime.MessageSender;
-import { DayReport } from './tempo/types';
 
 export const BACKEND_CONTENT_PORT_NAME = 'backend-to-content';
+
+export type LocalStorage = {
+  [keys in 'issueKey' | string]: any;
+};
 
 chrome.runtime.onMessage.addListener(
   (request: Message, _: MessageSender, sendResponse: (response: Message) => void) => {
@@ -21,36 +25,38 @@ function handleButtonClick(sendResponse: (response: Message) => void) {
   return (tab: Tab[]): void => {
     const activeTab = tab[0];
     if (activeTab.id && tabContains(TIMESHEET_URL, TIMESHEET_TITLE)(activeTab)) {
-      fillInReport(activeTab.id, sendResponse);
+      chrome.storage.sync.get(['issueKey'], fillInReport(activeTab.id, sendResponse));
     } else {
       sendResponse({ type: MessageType.FAILURE });
     }
   };
 }
 
-function fillInReport(tabId: number, sendResponse: (response: Message) => void): void {
-  const portToContent = chrome.tabs.connect(tabId, { name: BACKEND_CONTENT_PORT_NAME });
-  portToContent.postMessage({ type: MessageType.REQUEST_DATE_RANGE } as Message);
+function fillInReport(tabId: number, sendResponse: (response: Message) => void) {
+  return (storage: LocalStorage) => {
+    const portToContent = chrome.tabs.connect(tabId, { name: BACKEND_CONTENT_PORT_NAME });
+    portToContent.postMessage({ type: MessageType.REQUEST_DATE_RANGE } as Message);
 
-  portToContent.onMessage.addListener(async (message: Message, port) => {
-    switch (message.type) {
-      case MessageType.RESPOND_DATE_RANGE:
-        const reports = await getReports(message);
-        port.postMessage({ type: MessageType.FILL_IN_REPORT, payload: reports } as Message);
-        break;
-      case MessageType.SUCCESS:
-        sendResponse(message);
-        port.disconnect();
-        break;
-      default:
-        sendResponse({ type: MessageType.FAILURE });
-        port.disconnect();
-        break;
-    }
-  });
+    portToContent.onMessage.addListener(async (message: Message, port) => {
+      switch (message.type) {
+        case MessageType.RESPOND_DATE_RANGE:
+          const reports = await getReports(storage.issueKey, message.payload as string);
+          port.postMessage({ type: MessageType.FILL_IN_REPORT, payload: reports } as Message);
+          break;
+        case MessageType.SUCCESS:
+          sendResponse(message);
+          port.disconnect();
+          break;
+        default:
+          sendResponse({ type: MessageType.FAILURE });
+          port.disconnect();
+          break;
+      }
+    });
+  };
 }
 
-function getReports(message: Message): Promise<DayReport[]> {
-  const { from, to } = parseBoundaries(message.payload as string);
-  return getDayReportsFromTempo(288, newDayRange(from, to));
+function getReports(issueKey: number, dateText: string): Promise<DayReport[]> {
+  const { from, to } = parseBoundaries(dateText);
+  return getDayReportsFromTempo(issueKey, newDayRange(from, to));
 }
--- src/popup.ts
+++ src/popup.ts
@@ -1,16 +1,32 @@
 import { Message, MessageType } from './messages';
+import { LocalStorage } from './background';
 
-document.getElementById('button')?.addEventListener('click', () => {
-  chrome.runtime.sendMessage({ type: MessageType.BUTTON_CLICK } as Message, (response: Message): void => {
-    switch (response.type) {
-      case MessageType.SUCCESS:
-        console.log('Response was SUCCESS');
-        break;
-      case MessageType.FAILURE:
-        console.log('Response was FAILURE');
-        break;
-      default:
-        console.log('unknown response');
+document.querySelector('button#save-issue-key')?.addEventListener('click', () => {
+  const issueKey = (document.querySelector('input#tempo-issue-key') as HTMLInputElement).value;
+  chrome.storage.sync.set({ issueKey });
+});
+
+chrome.storage.sync.get(['issueKey'], (result: LocalStorage) => {
+  const input = document.querySelector('input#tempo-issue-key') as HTMLInputElement;
+  if (input) {
+    input.value = result.issueKey;
+  }
+});
+
+document.querySelector('button#fill-timesheet')?.addEventListener('click', () => {
+  chrome.runtime.sendMessage(
+    { type: MessageType.BUTTON_CLICK } as Message,
+    (response: Message): void => {
+      switch (response.type) {
+        case MessageType.SUCCESS:
+          console.log('Response was SUCCESS');
+          break;
+        case MessageType.FAILURE:
+          console.log('Response was FAILURE');
+          break;
+        default:
+          console.log('unknown response');
+      }
     }
-  });
+  );
 });
